b200bb7565396a8631074136639a0786ac45f062
pryrotech <colbypry@gmail.com>
2021-07-02 15:25:25 -0230

    Added database functionality

--- DrawnToYou/src/main/java/com/algonquin/drawntoyou/user/SignUpDatabase.java
+++ DrawnToYou/src/main/java/com/algonquin/drawntoyou/user/SignUpDatabase.java
@@ -7,17 +7,19 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SignUpDatabase {
-   static final String DB_URL = "jdbc:mysql://localhost/";
-   static final String USER = "guest";
-   static final String PASS = "guest123";
-   private String username;
-   private String password;
-   private String verificationCode;
+   static final String DB_URL = "jdbc:mysql://localhost:3306/";
+   static final String DB_URL_WITH_TABLE = "jdbc:mysql://localhost:3306/SIGN_UP_DATABASE";
+   static final String USER = "administrator";
+   static final String PASS = "rootPasswordDrawn2You";
+   static final String DB = "SIGN_UP_DATABASE";
+   private static String username;
+   private static String password;
+   private static String verificationCode;
    
    public SignUpDatabase() {
-	   username = "";
-	   password = "";
-	   verificationCode = "";
+	   username = "100";
+	   password = "1000";
+	   verificationCode = "10000";
    }
    
    public String getUsername() {
@@ -54,7 +56,7 @@ public class SignUpDatabase {
       try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
          Statement stmt = conn.createStatement();
       ) {		      
-         String sql = "CREATE DATABASE SIGN_UP_DATABASE";
+         String sql = "CREATE DATABASE SIGN_UP_DATABASE;";
          stmt.executeUpdate(sql);
          System.out.println("Database created successfully...");   	  
       } catch (SQLException e) {
@@ -62,11 +64,10 @@ public class SignUpDatabase {
          e.printStackTrace();
       } 
       
-      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      try(Connection conn = DriverManager.getConnection(DB_URL_WITH_TABLE, USER, PASS);
     	         Statement stmt = conn.createStatement();
     	      ) {		      
-    	         String sql = "CREATE TABLE SIGN_UP_INFO("
-    	         		+ "USERNAME VARCHAR(20),PASSWORD VARCHAR(20),VERIFICATION_CODE VARCHAR(4);";
+    	         String sql = "CREATE TABLE SIGN_UP_INFO( USERNAME VARCHAR(20),PASSWORD VARCHAR(20),VERIFICATION_CODE VARCHAR(4));";
     	         stmt.executeUpdate(sql);
     	         System.out.println("Table created successfully....");   	  
     	      } catch (SQLException e) {
@@ -74,17 +75,33 @@ public class SignUpDatabase {
     	         e.printStackTrace();
     	      } 
       
-      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
- 	         Statement stmt = conn.createStatement();
- 	      ) {		      
- 	         String sql = "CREATE TABLE SIGN_UP_INFO("
- 	         		+ "USERNAME VARCHAR(20),PASSWORD VARCHAR(20),VERIFICATION_CODE VARCHAR(4);";
- 	         stmt.executeUpdate(sql);
- 	         System.out.println("Table created successfully....");   	  
- 	      } catch (SQLException e) {
- 	    	  System.out.println("The table has either been created,\nor there was an error.");
- 	         e.printStackTrace();
- 	      }
+      try(Connection conn = DriverManager.getConnection(DB_URL_WITH_TABLE, USER, PASS);
+    		  
+  	      ) {		      
+    	  String query = " INSERT INTO SIGN_UP_INFO (USERNAME,PASSWORD,VERIFICATION_CODE)"+ " values (?, ?, ?)";
+    	   PreparedStatement preparedStmt = conn.prepareStatement(query);
+    	      preparedStmt.setString (1, username);
+    	      preparedStmt.setString (2, password);
+    	      preparedStmt.setString   (3, verificationCode);
+    	      preparedStmt.execute();
+  	         System.out.println("Values have been added to the table....");   	  
+  	      } catch (SQLException e) {
+  	    	  System.out.println("The user has either been created,\nor there was an error.");
+  	         e.printStackTrace();
+  	      }
+      
+      
+      try(Connection conn = DriverManager.getConnection(DB_URL_WITH_TABLE, USER, PASS);
+   	         Statement stmt = conn.createStatement();
+   	      ) {		      
+   	         String sql = "SELECT * FROM SIGN_UP_INFO;";
+   	         stmt.executeUpdate(sql);
+   	         System.out.println(stmt);   	  
+   	      } catch (SQLException e) {
+   	    	  System.out.println("The user has either been created,\nor there was an error.");
+   	         e.printStackTrace();
+   	      }
+      
 }
 
 
--- DrawnToYou/src/main/java/com/algonquin/drawntoyou/user/SignUpServlet.java
+++ DrawnToYou/src/main/java/com/algonquin/drawntoyou/user/SignUpServlet.java
@@ -3,6 +3,7 @@ package com.algonquin.drawntoyou.user;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
@@ -23,9 +24,14 @@ import javax.servlet.http.*;
 @WebServlet("/SignUpServlet")
 public class SignUpServlet extends HttpServlet
 {
+
 	
 	private static final long serialVersionUID = 1L;
-
+	  static final String DB_URL = "jdbc:mysql://localhost:3306/";
+	  static final String DB_URL_WITH_TABLE = "jdbc:mysql://localhost:3306/SIGN_UP_DATABASE";
+	  static final String USER = "administrator";
+	  static final String PASS = "rootPasswordDrawn2You";
+	  static final String DB = "SIGN_UP_DATABASE";
 
 
 
@@ -60,12 +66,70 @@ public class SignUpServlet extends HttpServlet
 	    request.setAttribute("password",password);
 	    request.setAttribute("verificationCode", verificationCode);
 	    
-	    request.getRequestDispatcher("/jsp.jsp").forward(request, response);
+	    request.getRequestDispatcher("/SignUp.jsp").forward(request, response);
 	    
 	    SignUpDatabase sendInfo = new SignUpDatabase();
 	    sendInfo.setUsername(username);
 	    sendInfo.setUsername(password);
 	    sendInfo.setUsername(verificationCode);
+	    
+		 try {
+	         Class.forName("com.mysql.jdbc.Driver");
+	         
+	     } 
+		 catch (ClassNotFoundException e) {
+	         System.out.println("Missing driver");
+	         e.printStackTrace();
+	     }
+	    
+	      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	    	         Statement stmt = conn.createStatement();
+	    	      ) {		      
+	    	         String sql = "CREATE DATABASE SIGN_UP_DATABASE;";
+	    	         stmt.executeUpdate(sql);
+	    	         System.out.println("Database created successfully...");   	  
+	    	      } catch (SQLException e) {
+	    	    	  System.out.println("The database has either been created,\nor there was an error.");
+	    	         e.printStackTrace();
+	    	      } 
+	    	      
+	    	      try(Connection conn = DriverManager.getConnection(DB_URL_WITH_TABLE, USER, PASS);
+	    	    	         Statement stmt = conn.createStatement();
+	    	    	      ) {		      
+	    	    	         String sql = "CREATE TABLE SIGN_UP_INFO( USERNAME VARCHAR(20),PASSWORD VARCHAR(20),VERIFICATION_CODE VARCHAR(4));";
+	    	    	         stmt.executeUpdate(sql);
+	    	    	         System.out.println("Table created successfully....");   	  
+	    	    	      } catch (SQLException e) {
+	    	    	    	  System.out.println("The table has either been created,\nor there was an error.");
+	    	    	         e.printStackTrace();
+	    	    	      } 
+	    	      
+	    	      try(Connection conn = DriverManager.getConnection(DB_URL_WITH_TABLE, USER, PASS);
+	    	    		  
+	    	  	      ) {		      
+	    	    	  String query = " INSERT INTO SIGN_UP_INFO (USERNAME,PASSWORD,VERIFICATION_CODE)"+ " values (?, ?, ?)";
+	    	    	   PreparedStatement preparedStmt = conn.prepareStatement(query);
+	    	    	      preparedStmt.setString (1, username);
+	    	    	      preparedStmt.setString (2, password);
+	    	    	      preparedStmt.setString   (3, verificationCode);
+	    	    	      preparedStmt.execute();
+	    	  	         System.out.println("Values have been added to the table....");   	  
+	    	  	      } catch (SQLException e) {
+	    	  	    	  System.out.println("The user has either been created,\nor there was an error.");
+	    	  	         e.printStackTrace();
+	    	  	      }
+	    	      
+	    	      
+	    	      try(Connection conn = DriverManager.getConnection(DB_URL_WITH_TABLE, USER, PASS);
+	    	   	         Statement stmt = conn.createStatement();
+	    	   	      ) {		      
+	    	   	         String sql = "SELECT * FROM SIGN_UP_INFO;";
+	    	   	         stmt.executeUpdate(sql);
+	    	   	         System.out.println(stmt);   	  
+	    	   	      } catch (SQLException e) {
+	    	   	    	  System.out.println("The user has either been created,\nor there was an error.");
+	    	   	         e.printStackTrace();
+	    	   	      }
 	}
 	
 
