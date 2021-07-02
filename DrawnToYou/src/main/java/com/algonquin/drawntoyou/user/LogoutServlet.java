a9737bbfa71c18ebc4af599d1dabe3d0c57e5691
blguerin <84094009+blguerin@users.noreply.github.com>
2021-07-02 16:38:08 -0400

    Update LogoutServlet.java
    Login, direct to Profile, Logout functional

--- DrawnToYou/src/main/java/com/algonquin/drawntoyou/user/LogoutServlet.java
+++ DrawnToYou/src/main/java/com/algonquin/drawntoyou/user/LogoutServlet.java
@@ -28,21 +28,23 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
+		
+		/*HttpSession session = request.getSession(false);
 		if (session != null) {
 			session.removeAttribute("user");
 			
-			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
 			dispatcher.forward(request, response);
-		}
+		} */
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.sendRedirect("login.jsp");
+		//doGet(request, response);
 	}
 
 }
+
